7673485c1df48cefb145c7072655a9cc7d7fc202
sandorZHAO <757355084@qq.com>
2021-03-27 23:39:28 +0800

    :construction: button 功能
    1. button (default,primary,info,success,error,warning)
    2. button disabled
    3. 点击功能

--- dev/app.vue
+++ dev/app.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="app">
+        hello world
+        <hr>
         <IoHelloWorld/>
+        <hr>
+
+        <br>
+        <br>
+        icon
+        <hr>
         <IoIcon/>
         <IoIcon name="code"/>
         <IoIcon size="14px" disabled/>
@@ -9,8 +17,6 @@
         <IoIcon name="load1"/>
         <IoIcon name="load2"/>
 
-        <br>
-        <br>
         <IoIcon name="bong"/>
         <IoIcon name="grip-vertical"/>
         <IoIcon name="sign-language"/>
@@ -21,6 +27,35 @@
         <IoIcon name="sign-out-alt"/>
 
         <IoIcon color="red"/>
+        <hr>
+
+        <br>
+        <br>
+        button
+        <hr>
+        <br>
+        <IoButton>hello</IoButton>
+        <IoButton disabled>hello</IoButton>
+        <br>
+        <IoButton type="primary">hello</IoButton>
+        <IoButton type="primary" disabled>hello</IoButton>
+        <br>
+        <IoButton type="info">hello</IoButton>
+        <IoButton type="info" disabled>hello</IoButton>
+        <br>
+        <IoButton type="success">hello</IoButton>
+        <IoButton type="success" disabled>hello</IoButton>
+        <br>
+        <IoButton type="error">hello</IoButton>
+        <IoButton type="error" disabled>hello</IoButton>
+        <br>
+        <IoButton type="warning">hello</IoButton>
+        <IoButton type="warning" disabled>hello</IoButton>
+        <br>
+        <IoButton @click="clickCount(cnt) ">{{ cnt }}</IoButton>
+        <IoButton @click="clickCount1(cnt1)" disabled>{{ cnt1 }}</IoButton>
+
+        <hr>
     </div>
 </template>
 
@@ -28,7 +63,18 @@
 export default {
     name: 'app',
     data () {
-        return {}
+        return {
+            cnt: 1,
+            cnt1: 1
+        }
+    },
+    methods: {
+        clickCount (i) {
+            this.cnt = i + 1
+        },
+        clickCount1 (i) {
+            this.cnt1 = i + 1
+        }
     }
 }
 </script>
---
+++ packages/IoButton/src/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <button class="io-button"
+            @click="handleClick"
+            :class="[
+            type ? 'io-button-' + type: '',
+            {
+                'is-disabled':disabled
+            }]">
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'IoButton',
+    props: {
+        type: {
+            type: String,
+            default: 'default'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        handleClick () {
+            if (!this.disabled) {
+                this.$emit('click')
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import 'index.scss';
+</style>
